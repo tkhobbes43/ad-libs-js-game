36b030a5b23274628f4f3b9b35473eb908301c9d
Tommy Lee <t.k.hobbes43@gmail.com>
2022-04-17 00:10:59 -0500

    added comments to the .js, and changed the story

--- js/story.js
+++ js/story.js
@@ -1,8 +1,10 @@
-
+// Declare variables and capture input. //
 const givenNoun = prompt("Please provide a noun.");
 const givenVerb = prompt("Please provide a verb.");
 const givenAdjective = prompt("Please provide an adjective.");
 
-const crazyMessage = `<p> The ${givenAdjective} programmer decided to ${givenVerb} to the ${givenNoun}.<p>`
+// combine the input with other words to create a story. //
+const crazyMessage = `<p> One day there was a ${givenNoun} who really needed to ${givenVerb}.  Given that the ${givenNoun} was very ${givenAdjective}, the ${givenNoun} had no choice but to go home.<p>`
 
+// this will display they story as a <p> inside the <main> element. //
 document.querySelector('main').innerHTML = crazyMessage;
