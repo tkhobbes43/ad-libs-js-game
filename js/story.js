6f97e0e9f9e0f72cd15676e3facf0aa7261ba230
Tommy Lee <t.k.hobbes43@gmail.com>
2022-04-17 00:21:25 -0500

    changed the story

--- js/story.js
+++ js/story.js
@@ -4,7 +4,7 @@ const givenVerb = prompt("Please provide a verb.");
 const givenAdjective = prompt("Please provide an adjective.");
 
 // combine the input with other words to create a story. //
-const crazyMessage = `<p> One day there was a ${givenNoun} who really needed to ${givenVerb}.  Given that the ${givenNoun} was very ${givenAdjective}, the ${givenNoun} had no choice but to go home.<p>`
+const crazyMessage = `<p> Our school cafeteria has really ${givenAdjective} food.  Just thinking about it makes my stomach ${givenVerb}.  The spaghetti is ${givenAdjective} and tastes like ${givenNoun}!<p>`
 
 // this will display they story as a <p> inside the <main> element. //
 document.querySelector('main').innerHTML = crazyMessage;
